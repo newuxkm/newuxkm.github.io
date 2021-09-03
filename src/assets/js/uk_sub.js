2caae7c111d52767e94e222bb61e69e17cfaaeaa
uxkm <uxkmtemp@gmail.com>
2021-09-03 17:21:00 +0900

    uxkm 세팅

--- src/assets/js/uk_sub.js
+++ src/assets/js/uk_sub.js
@@ -1,10 +1,14 @@
 
-// side menu -----------------------------------------------------------------------------------------------------------
+// selector ------------------------------------------------------------------------------------------------------------
+const _top_link = $('.top_link ul');
 const _side_name = $('.side_menu');
 const _side_menu_d3 = _side_name.find('.depth3 > li');
+
+
+// side menu -----------------------------------------------------------------------------------------------------------
+const tg_on = 'tg_on';
 _side_menu_d3.each(function(i, e){
   if( _side_name.is('.pub_side_nemu') ){
-    const tg_on = 'tg_on';
     if( $(e).children().length > 1 ){
       $(e).is('.active') ? $(e).addClass(tg_on) : false;
       $(e).addClass('toggle_box');
@@ -18,10 +22,29 @@ _side_menu_d3.each(function(i, e){
 });
 
 
-
-
-//scroll ---------------------------------------------------------------------------------------------------------------
+// window scroll -------------------------------------------------------------------------------------------------------
+let top_link_offset = _top_link.offset().top;
 $(window).on('scroll', function(){
   let sct = $(window).scrollTop();
   hd_common(sct);
+
+  sct > top_link_offset - _ukHeader.height() ?  _top_link.addClass('fixed') : _top_link.removeClass('fixed');
 }).trigger('scroll');
+
+
+// window resize -------------------------------------------------------------------------------------------------------
+$(window).on('resize', function(){
+ let win_w = $(window).width();
+
+ if( win_w > md_size ){
+   _side_name.find('.depth4').removeAttr('style');
+   if( _side_menu_d3.is('.active') ){
+     _side_name.find('.depth3 > li.active').addClass(tg_on);
+   }
+ }
+ else{
+   if( _side_menu_d3.is('.active') ){
+     _side_menu_d3.removeClass(tg_on);
+   }
+ }
+}).trigger('resize');
