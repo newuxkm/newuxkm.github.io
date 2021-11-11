3db5b746262c6e6ec308c22a5d41ceb40e7b34b5
uxkm <uxkmtemp@gmail.com>
2021-11-11 16:48:46 +0900

    uxkm 세팅

--- src/assets/js/uk_common.js
+++ src/assets/js/uk_common.js
@@ -1,8 +1,9 @@
 
 // variable ------------------------------------------------------------------------------------------------------------
-const scroll_size = 17;
-const screen_sm_max = 767 - scroll_size;
-const screen_md_max = 993 - scroll_size;
+const scroll_width = window.innerWidth - $(window).width();
+const screen_lg_max = 1201 - scroll_width;
+const screen_md_max = 993 - scroll_width;
+const screen_sm_max = 767 - scroll_width;
 
 // selector ------------------------------------------------------------------------------------------------------------
 const _window = $(window);
@@ -16,7 +17,7 @@ const _ukSkip = $('.uk_skip');
 // header selector
 const _ukHeader = $('.uk_header');
 const _ukNavArea = _ukHeader.find('.hd_nav_area');
-const _ukNav= _ukNavArea.find('.nav');
+const _ukNav = _ukNavArea.find('.nav');
 const search_btn = 'search_btn';
 const sitemap_btn = 'sitemap_btn';
 const search_close = 'search_close';
@@ -42,12 +43,10 @@ const _topLink = $('.top_link');
 const ukEditor = 'uk_editor';
 const result_wrap = 'result_wrap';
 const browserTitle = 'browserTitle';
-const focus_el = 'a, button, input, select, textarea';
+const focus_el = 'a, button, input, select, textarea, #content_title';
 
 // class name
 const hd_progress = 'hd_progress';
-const hd_sitemap_on = 'hd_sitemap_on';
-const hd_search_on = 'hd_search_on';
 const hd_layer_open = 'hd_layer_open';
 
 // variable
@@ -339,7 +338,7 @@ function focus_controll(){
 
   // focus 요소 text 확인
   $('*').focus(function(){
-    console.log( $(this).text() );
+    //console.log( $(this).text() );
   });
 }
 
